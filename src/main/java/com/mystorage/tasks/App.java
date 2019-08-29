aab29acf9d5440d1f6330ea6ef3e45071df177c4
Arvio <arvio@yandex.ru>
2019-08-29 13:38:15 +0300

    Initial commit

---
+++ src/main/java/com/mystorage/tasks/App.java
@@ -0,0 +1,13 @@
+package com.mystorage.tasks;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
