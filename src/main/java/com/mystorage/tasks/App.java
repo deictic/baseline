a4b61688eb2c8e4084d1a5ed17ff7d3cc6833aa2
Arvio <arvio@yandex.ru>
2019-08-29 14:07:30 +0300

    test commit

--- src/main/java/com/mystorage/tasks/App.java
+++ src/main/java/com/mystorage/tasks/App.java
@@ -1,13 +1,24 @@
 package com.mystorage.tasks;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        boolean exit = false;
+        while (!exit) {
+            System.out.println("Please input a line");
+            String line = scanner.nextLine();
+            System.out.printf("User input was: %s%n", line);
+
+            if (line.equals("exit")) exit = true;
+        }
     }
 }
