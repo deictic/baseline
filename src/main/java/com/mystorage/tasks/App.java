fa904e19e950664045967712075091a99c43675a
Arvio <arvio@yandex.ru>
2019-09-03 09:45:18 +0300

    - finishing touches

--- src/main/java/com/mystorage/tasks/App.java
+++ src/main/java/com/mystorage/tasks/App.java
@@ -4,10 +4,6 @@ import com.mystorage.tasks.services.ProductService;
 
 import java.util.Scanner;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args ) {
@@ -21,30 +17,34 @@ public class App
             System.out.println("Please input a request");
 
             String inputLine = scanner.nextLine();
-            if (inputLine.equals("exit")) exit = true;
-            else {
-                String[] request = inputLine.split(" ");
 
-                String requestResult;
-                switch (request[0]) {
-                    case "NEWPRODUCT":
-                        requestResult = productService.newProduct(request);
-                        break;
-                    case "PURCHASE":
-                        requestResult = productService.purchase(request);
-                        break;
-                    case "DEMAND":
-                        requestResult = productService.demand(request);
-                        break;
-                    case "SALSEREPORT":
-                        requestResult = productService.salesReport(request);
-                        break;
-                    default:
-                        requestResult = "ERROR";
+            String requestResult = "";
+
+            try {
+                if (inputLine.equals("EXIT")) exit = true;
+                else {
+                    String[] request = inputLine.split(" ");
+
+                    switch (request[0]) {
+                        case "NEWPRODUCT":
+                            requestResult = productService.newProduct(request);
+                            break;
+                        case "PURCHASE":
+                            requestResult = productService.purchase(request);
+                            break;
+                        case "DEMAND":
+                            requestResult = productService.demand(request);
+                            break;
+                        case "SALESREPORT":
+                            requestResult = productService.salesReport(request);
+                            break;
+                        default:
+                            requestResult = "ERROR";
+                    }
                 }
+            } catch (Throwable t) { requestResult = "ERROR"; }
 
-                System.out.println(requestResult);
-            }
+            System.out.println(requestResult);
 
         }
     }
--- src/test/java/com/mystorage/tasks/AppTest.java
+++ src/test/java/com/mystorage/tasks/AppTest.java
@@ -4,17 +4,14 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
+
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
+    public void mainAppTest() {
+
+        //not much to test here
         assertTrue( true );
     }
 }
